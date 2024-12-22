2c77f7c08575a205512af5ff3304ba476df8ed82
Yahya Qadeer Dar <yahyaqdar@gmail.com>
2024-12-22 19:54:24 +0500

    Update client.cpp

--- client.cpp
+++ client.cpp
@@ -3,77 +3,73 @@
 // ......
 // ......
 #include <iostream>
-#include <cstring>
 #include <winsock2.h>
 #include <thread>
-
+#include <string>
+using namespace std;
 
 #pragma comment(lib, "ws2_32.lib")
-using namespace std;
 #define PORT 8080
 
-void handle_server_response(SOCKET clientSocket) {
-    char buffer[1024];
-    int bytes_received;
+// GUI Functions
+void printBanner() {
+// Updated ASCII Art for Server and Client with proper escape sequences
+cout << "\033[1;36m" << R"(
+  _____  _    _           _____ ______             _____ 
+ |  __ \| |  | |   /\    / ____|  ____|           / ____|
+ | |__) | |__| |  /  \  | (___ | |__             | |     
+ |  ___/|  __  | / /\ \  \___ \|  __|            | |     
+ | |    | |  | |/ ____ \ ____) | |____           | |____ 
+ |_|    |_|  |_/_/    \_\_____/|______|           \_____|
+                                         ______          
+                                        |______|         
+)" << "\033[0m\n";
 
-    while (true) {
-        memset(buffer, 0, sizeof(buffer));
-        bytes_received = recv(clientSocket, buffer, sizeof(buffer), 0);
-
-        if (bytes_received <= 0) {
-            cout << "Disconnected from server.\n";
-            break;
-        }
-
-        cout << "Server: " << buffer << endl;
-    }
+    cout << "\033[1;33m<<<<< Welcome to the PHASE_C Chat Client >>>>>\033[0m\n" << endl;
 }
 
+void printInstructions() {
+    cout << "\033[1;36mInstructions:\033[0m\n"
+              << " - Type your messages and press Enter to send.\n"
+              << " - Type 'exit' to leave the chat.\n"
+              << "\033[1;30m------------------------------------------------------------\033[0m\n";
+}
 
+void printSentMessage(const string &message) {
+    cout << "\033[1;32m[You]: \033[0m" << message << endl;
+}
+
+void printReceivedMessage(const string &message) {
+    cout << "\033[1;34m" << message << "\033[0m" << endl;
+}
 
 int main() {
-    WSADATA wsa_data;
-    if (WSAStartup(MAKEWORD(2, 2), &wsa_data) != 0) {
-        cerr << "WSAStartup failed.\n";
-        return -1;
-    }
-    // initializing the socket
+    printBanner();
+    printInstructions();
+
     SOCKET clientSocket = socket(AF_INET, SOCK_STREAM, 0);
-    if (clientSocket == INVALID_SOCKET) {
-        cerr << "Socket creation failed.\n";
-        WSACleanup();
-        return -1;
-    }
-    //setting up the socket
-    sockaddr_in serverAddr;
-    serverAddr.sin_family = AF_INET;
-    serverAddr.sin_port = htons(PORT);
+    sockaddr_in serverAddr = {AF_INET, htons(PORT), 0};
     serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
 
-    if (connect(clientSocket, (struct sockaddr*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR) {
-        cerr << "Connection failed.\n";
-        closesocket(clientSocket);
-        WSACleanup();
-        return -1;
-    }
-    //sucesfully connected
-    cout << "Connected to server.\n";
+    connect(clientSocket, (sockaddr *)&serverAddr, sizeof(serverAddr));
 
-    // Thread to handle server responses
-    thread(handle_server_response, clientSocket).detach();
-    //messade (bufer) stream
+    cout << "Enter your name: ";
+    string name;
+    getline(cin, name);
+    send(clientSocket, name.c_str(), name.length(), 0);
+
+    thread(handleServerResponse, clientSocket).detach();
+
     char buffer[1024];
     while (true) {
-        cout << "Enter message: ";
         cin.getline(buffer, sizeof(buffer));
-
         if (string(buffer) == "exit") {
             break;
         }
-
         send(clientSocket, buffer, strlen(buffer), 0);
+        printSentMessage(buffer);
     }
-    //closing the socket
+
     closesocket(clientSocket);
     WSACleanup();
     return 0;
