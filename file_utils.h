014eea84c4e3c4ddc9afe36914ae75a00ce5704c
rayy1704 <ryaansalman@gmail.com>
2024-12-22 18:24:30 +0500

    add the file_utilities

---
+++ file_utils.h
@@ -0,0 +1,16 @@
+#ifndef FILE_UTILS_H
+#define FILE_UTILS_H
+
+#include <fstream>
+#include <string>
+
+class FileUtils {
+public:
+    static void appendToFile(const std::string &filename, const std::string &data) {
+        std::ofstream file(filename, std::ios::app);
+
+        }
+    }
+};
+
+#endif
