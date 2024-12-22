199adc1a581868a9c1a22355faf00a96be6bec87
Yahya Qadeer Dar <yahyaqdar@gmail.com>
2024-12-22 23:10:37 +0500

    Update client.cpp

--- client.cpp
+++ client.cpp
@@ -61,48 +61,35 @@ void handleServerResponse(SOCKET clientSocket) {
 
 
 int main() {
-    WSADATA wsa_data;
-    if (WSAStartup(MAKEWORD(2, 2), &wsa_data) != 0) {
-        cerr << "WSAStartup failed.\n";
-        return -1;
-    }
-    // initializing the socket
+    printBanner();
+    printInstructions();
+
+    WSADATA wsaData;
+    WSAStartup(MAKEWORD(2, 2), &wsaData);
+
     SOCKET clientSocket = socket(AF_INET, SOCK_STREAM, 0);
-    if (clientSocket == INVALID_SOCKET) {
-        cerr << "Socket creation failed.\n";
-        WSACleanup();
-        return -1;
-    }
-    //setting up the socket
-    sockaddr_in serverAddr;
-    serverAddr.sin_family = AF_INET;
-    serverAddr.sin_port = htons(PORT);
+    sockaddr_in serverAddr = {AF_INET, htons(PORT), 0};
     serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
 
-    if (connect(clientSocket, (struct sockaddr*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR) {
-        cerr << "Connection failed.\n";
-        closesocket(clientSocket);
-        WSACleanup();
-        return -1;
-    }
-    //sucesfully connected
-    cout << "Connected to server.\n";
+    connect(clientSocket, (sockaddr *)&serverAddr, sizeof(serverAddr));
 
-    // Thread to handle server responses
-    thread(handle_server_response, clientSocket).detach();
-    //messade (bufer) stream
+    cout << "Enter your name: ";
+    string name;
+    getline(cin, name);
+    send(clientSocket, name.c_str(), name.length(), 0);
+
+    thread(handleServerResponse, clientSocket).detach();
+
     char buffer[1024];
     while (true) {
-        cout << "Enter message: ";
         cin.getline(buffer, sizeof(buffer));
-
         if (string(buffer) == "exit") {
             break;
         }
-
         send(clientSocket, buffer, strlen(buffer), 0);
+        printSentMessage(buffer);
     }
-    //closing the socket
+
     closesocket(clientSocket);
     WSACleanup();
     return 0;
